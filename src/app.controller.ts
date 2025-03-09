8730690f986876e76864028f7af6fd1e0393cce8
facundo <cabrera.facundo990@gmail.com>
2025-03-08 22:09:44 -0300

    bruma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { User } from '@prisma/client';
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
 
 @Controller()
 export class AppController {
@@ -14,5 +16,14 @@ export class AppController {
   @Get('test')
   getTest(): Promise<User[]> {
     return this.appService.getTest();
+  }
+
+  @Post()
+  async example(@Res() res: Response) {
+    const result = streamText({
+      model: openai('gpt-4o'),
+    });
+
+    result.pipeDataStreamToResponse(res);
   }
 }
